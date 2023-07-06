0f3646081a0c8a9cb5616b161e7d651d51076be9
sanghamitra0591 <sanghamitramymail@gmail.com>
2023-07-06 14:48:17 +0530

    updated routes

--- Routes/User.Route.js
+++ Routes/User.Route.js
@@ -40,7 +40,7 @@ UserRouter.post("/signup", async(req, res)=>{
                 await newData.save();
                 res.send({"msg": "Successfully signed up"})
             }else {
-                res.send({"error": err})
+                res.send({"err": err})
                 console.log(err);
             }
         })
